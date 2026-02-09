5b7cc4f611933d5164f29ec54bce32b602f72f93
Peter Kaagman <prjv.kaagman@gmail.com>
2026-02-09 14:55:10 +0100

    feat(issue #38): add Log4perl + ErrorHandler + error handling refactor

--- lib/MySite/Index.pm
+++ lib/MySite/Index.pm
@@ -7,16 +7,31 @@ use Data::Dumper;
 use Template;
 use FindBin;
 use MySite::Utils qw(render_markdown);
+use MySite::ErrorHandler qw(db_guard template_error);
 
 
 sub _index {
-    my $articles = schema->resultset('Article')->search(
-        {},
-    {
-        order_by => {'-desc' => ['created']},
-    }
+    my ($db_ok, $articles) = db_guard(
+      action => 'fetch articles for homepage',
+      user => session->read('user'),
+      code => sub {
+        return schema->resultset('Article')->search(
+          {},
+          { order_by => {'-desc' => ['created']} }
+        );
+      }
     );
-    # debug Dumper $articles;
+    
+    unless ($db_ok) {
+      return template_error(
+        title => 'Database Error',
+        error => 'Could not load articles',
+        status => 500
+      );
+    }
+    
+    debug "Found " . ($articles ? $articles->count : 0) . " articles";
+    
     template 'article/list' => {
         'title' => 'MySite',
         'user' => session->read('user'),
