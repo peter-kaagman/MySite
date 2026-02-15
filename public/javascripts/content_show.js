eeaeedb314dd3e4c2d844e12bce8bcd7e1473d05
Peter Kaagman <prjv.kaagman@gmail.com>
2026-02-15 11:37:40 +0100

    Verbeteren van Markdown parsing Fixes #48

---
+++ public/javascripts/content_show.js
@@ -0,0 +1,6 @@
+// content_show.js - Entry point voor content weergave (highlighting & copy)
+import { initCodeblockTools } from './modules/codeblock_tools.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initCodeblockTools();
+});
