e50fc118f6de3564d019a7247c0fd7bcde22f918
Peter Kaagman <prjv.kaagman@gmail.com>
2026-02-27 10:58:56 +0100

    Aanpassing docker build script

--- tools/rebuild_prod.sh
+++ tools/rebuild_prod.sh
@@ -12,14 +12,14 @@ fi
 
 if [ "$1" == "--all" ]; then
   echo "Rebuilding ALL containers..."
-  docker-compose -f docker-compose.prod.yml down
-  docker-compose -f docker-compose.prod.yml build
-  docker-compose -f docker-compose.prod.yml up -d
+  docker compose -f docker-compose.prod.yml down
+  docker compose -f docker-compose.prod.yml build
+  docker compose -f docker-compose.prod.yml up -d
   echo "Rebuild complete (alle containers)."
 else
   echo "Building MySite image..."
-  docker-compose -f docker-compose.prod.yml build mysite
+  docker compose -f docker-compose.prod.yml build mysite
   echo "Restarting MySite container..."
-  docker-compose -f docker-compose.prod.yml up -d mysite
+  docker compose -f docker-compose.prod.yml up -d mysite
   echo "Rebuild complete (alleen MySite)."
 fi
