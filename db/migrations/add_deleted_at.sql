b60d69dcf2765a4325c5831c0c4e81edba0a2d41
Peter Kaagman <prjv.kaagman@gmail.com>
2026-04-17 12:24:58 +0200

    opruimen

--- db/migrations/add_deleted_at.sql
+++
@@ -1,6 +0,0 @@
-SQLite version 3.45.1 2024-01-30 16:01:20
-Enter ".help" for usage hints.
-[?2004hsqlite> ^C[?2004l[?2004h
-[?2004l[?2004hsqlite> 
-[?2004l[?2004hsqlite> 
-[?2004l[?2004hsqlite> ^C[?2004l[?2004h[C[C[C[C[C[C[C[C^C[?2004l
